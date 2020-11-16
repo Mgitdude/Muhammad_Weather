e5577a18460b8741e9a21a61cf306338d0ac5e98
MUSgitudev <72415650+MUSgitudev@users.noreply.github.com>
2020-11-16 17:29:56 -0500

    Initial Commit

---
+++ Muhammad_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Muhammad_Weather
+//
+//  Created by MUS on 2020-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
